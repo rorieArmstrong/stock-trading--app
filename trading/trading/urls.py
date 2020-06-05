33554babb7d008c88c3666c9dea37d4cc8f0f7e8
igor <krzywakigor@gmail.com>
2020-06-05 15:57:06 +0100

    merged2nd

--- trading/trading/urls.py
+++ trading/trading/urls.py
@@ -18,10 +18,10 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    #path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('frontend.urls'), name="home"),
     path('admin/', admin.site.urls),
     path('api/', include('trader.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('frontend', include('frontend.urls'))
 ]
