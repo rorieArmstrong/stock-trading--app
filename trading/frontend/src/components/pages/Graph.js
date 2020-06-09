8c6f7c8a4c37cf5426707fb5d8e74be8b09dec43
ksitigarbha <58754415+karmicdribble@users.noreply.github.com>
2020-06-09 21:30:31 +0100

    added gets for watchlist

--- trading/frontend/src/components/pages/Graph.js
+++ trading/frontend/src/components/pages/Graph.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import TradingViewWidget, {Themes} from 'react-tradingview-widget'
+import { thatReturnsThis } from 'react-tradingview-widget/dist/vendor';
 
 class Graph extends Component {
 
+    //["AAPL","IBM","TSLA","AMD","MSFT","GOOG","FB","RMG",'NTDMF', 'RIG', 'RRC', 'COTY', 'SSL']
 
     constructor(props) {
         super(props);
         this._ref = React.createRef();
         this.state = {
-            watchlist: ["AAPL","IBM","TSLA","AMD","MSFT","GOOG","FB","RMG",'NTDMF', 'RIG', 'RRC', 'COTY', 'SSL']
+            watchlist: [],
+            userID:''
         }
     }
-    
+
+     componentDidMount() {
+        axios.get('/api/users')
+        .then((data) => {
+            this.setState({userID: data.data[0].id})
+            console.log(this.state.userID)
+        }, (error) => {
+            console.log(error);
+        }).then(axios.get('/api/stocks/?user=1')
+        .then((data) => {
+            this.setState({watchlist: data.data[0]})
+            console.log(this.state.watchlist)
+        }, (error) => {
+            console.log(error)
+        }))
+
+    }
+
     render() {
         return (
             <TradingViewWidget
