371e7701661bb7338e8bbbee19fcc1079c85f3e2
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2020-06-10 09:00:50 +0100

    add getting stock data from db

--- trading/frontend/src/components/pages/Portfolio.js
+++ trading/frontend/src/components/pages/Portfolio.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import { Link} from "react-router-dom";
+import axios from 'axios'
 
 class Portfolio extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: {}
+            data: []
         }
+    }
+
+    getUser = () => {
+        axios.get('/api/users')
+        .then(data => this.setState({userID: data.data[0].id}))
+        .catch(error => {console.log(error)})
+
     }
     
     onLoad = () => {
-        // Get data from api endpoint
+        axios.get('/api/users')
+        .then(data => { this.setState({userID: data.data[0].id}); return axios.get('/api/stocks/?user=' + data.data[0].id)})
+        .then(res => this.setState({data: res.data}))
+        .catch(error => {console.log(error)})
     }
 
     buy = () => {}
@@ -28,8 +39,20 @@ class Portfolio extends Component {
                     <h3>Value: {}</h3>
                     <h3>balance: {}</h3>
                     <Link to='/d/search'><button>Add Stock to Watchlist</button></Link>
-                    <button onClick={this.buy()}>buy</button>
-                    <button onClick={this.sell()}>Sell</button>
+                </div>
+                <div>
+                    {this.state.data.map(stock => {
+                        return(
+                            <div>
+                                <h3>{stock.stock_symbol}</h3>
+                                <p>Amout: {stock.stocks_bought_number}</p>
+                                <p>Bought at: {stock.bought_at_price}</p> 
+                                <p>Current Price: </p>
+                                <button onClick={this.buy(stock.id)}>buy</button>
+                                <button onClick={this.sell(stock.id)}>Sell</button>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         );
