f22cbf96f6f4e112ab22a627345cb92aa0bb85fb
huseyin <thehusko@gmail.com>
2020-06-11 09:57:21 +0100

    refactored portfolio view

--- trading/frontend/src/components/pages/Portfolio.js
+++ trading/frontend/src/components/pages/Portfolio.js
@@ -50,51 +50,40 @@ class Portfolio extends Component {
         return price
     }
 
-    buy = async (id, symbol, current) => {
+    buy = async (id, symbol, current) => { //id and current look useless?
         // buy an amount of stocks and update the balance and amount in database
         let price = this.state.prices[symbol]
         let amount = await prompt("How many stocks would you like to buy at " + price, 1);
         let cost = (amount*price)
         console.log(id, symbol, current, amount, this.state.userID, cost)
         if (cost <= this.state.balance){
-            axios.patch('api/users/3/',{
-                "balance": 10000
+            axios.patch(`/api/users/${this.state.userID}/`,{
+                "balance": 30
             }, {headers:{'Content-Type': 'application/json'}})
             .then(res => {console.log(res)})
             .catch(error => {console.log(error)})
-            if (current === 0){
-                axios.put('/api/stocks/'+id+'/',{
-                    "stock_symbol": symbol,
-                    "stocks_bought_number": amount,
-                    "bought_at_price": parseInt(price),
-                    "userID": this.state.userID
-                    }, {headers:{'Content-Type': 'application/json'}})
-                .then(res => {console.log(res)})
-                .catch(error => {console.log(error)})
-                window.alert('Stocks have been purchased')
-            }else{
-                axios.post('/api/stocks/',{
-                    "stock_symbol": symbol,
-                    "stocks_bought_number": amount,
-                    "bought_at_price": parseInt(price),
-                    "userID": this.state.userID
-                    }, {headers:{'Content-Type': 'application/json'}})
-                .then(res => {console.log(res)})
-                .catch(error => {console.log(error)})
+            
+            axios.post('/api/stocks/',{
+                "stock_symbol": symbol,
+                "stocks_bought_number": amount,
+                "bought_at_price": parseInt(price),
+                "userID": this.state.userID
+                }, {headers:{'Content-Type': 'application/json'}})
+            .then(res => {console.log(res)})
+            .catch(error => {console.log(error)})
                 // window.alert('Stocks have been purchased')
-            }
         }else{
             window.alert('You do not have enough funds in your account!')
         }
     }
 
-    sell = async (id, symbol, currnet) => {
+    sell = async (id, symbol, current) => {
         // sell an amount of stocks and update the balance and amount in database
     }
 
     remove = (id, amount) => {
         if (amount>0){
-            // sell all then delete
+            // sell all then delete e.g sell(amount) then axios.delete
         }else{
             axios.delete('/api/stocks/' + id + '/')
             .then(this.setState({updating: false}))
@@ -106,34 +95,73 @@ class Portfolio extends Component {
         this.onLoad()
     }
 
+    watchlist(){
+        const unique = [...new Set(this.state.data.map(stock => stock.stock_symbol))]
+        return(
+        unique.map(stock => {
+            return(
+                <React.Fragment>
+                <h3>{stock}</h3>
+                <p>Current Price: £{this.state.prices[stock]}</p>
+                <button onClick={() => this.buy(stock.id, stock, stock.stocks_bought_number)}>Buy</button>
+                </React.Fragment>
+            )
+        })
+        )
+    }
+
     render() {
         if(this.state.loading == true) {
             return <div> Loading </div>
         }
         console.log(this.state.data)
         return (
+            <React.Fragment>
             <div>
                 <div key='1'>
                     <h3>Value: {}</h3>
                     <h3>balance: {this.state.balance}</h3>
                     <Link to='/d/search'><button>Add Stock to Watchlist</button></Link>
                 </div>
+                <div>
+                    {this.watchlist()}
+                </div>
                 <div key='2'>
+                    <h2>Orders</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Symbol</th>
+                                <th>Amount</th>
+                                <th>Price</th>
+                                <th>Price of purchase</th>
+                                <th>Time of purchase</th>
+                            </tr>
+                        </thead>
+                        <tbody>
                     {this.state.data.map(stock => {
-                        return(
-                            <div>
-                                <h3>{stock.stock_symbol}</h3>
-                                <p>Amout: {stock.stocks_bought_number}</p>
-                                <p>Bought at: {stock.bought_at_price}</p> 
-                                <p>Approximate Price: {this.state.prices[stock.stock_symbol]}</p>
-                                <button onClick={() => this.buy(stock.id, stock.stock_symbol, stock.stocks_bought_number)}>Buy</button>
-                                <button onClick={() => this.sell(stock.id, stock.stock_symbol, stock.stocks_bought_number)}>Sell</button>
-                                <button onClick={() => this.remove(stock.id, stock.stocks_bought_number)}>Remove</button>
-                            </div>
-                        )
-                    })}
+                        if (stock.stocks_bought_number > 0)
+                            return (
+                            <tr key={stock.id}>
+                                <td>{stock.id}</td>
+                                <td>{stock.stock_symbol}</td>
+                                <td>{stock.stocks_bought_number}</td>
+                                <td>{this.state.prices[stock.stock_symbol]}</td>
+                                <td>{stock.bought_at_price}</td>
+                                <td>{stock.bought_at_time}</td>
+                                <td><button onClick={() => this.sell(stock.id, stock.stock_symbol, stock.stocks_bought_number)}>Sell</button></td>
+                                <td><button onClick={() => this.remove(stock.id, stock.stocks_bought_number)}>Close</button></td>
+                            </tr>
+                            )
+                        }
+                        
+                    )}
+                    </tbody>
+                    </table>
                 </div>
             </div>
+            </React.Fragment>
         );
     }
 }
